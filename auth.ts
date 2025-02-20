04bf5be70353c0c95d57084c4796c5914a76cc1a
francis.l <francis.l@binance.com>
2025-02-20 18:47:15 +0700

    1

--- auth.ts
+++ auth.ts
@@ -29,8 +29,10 @@ export const { auth, signIn, signOut } = NextAuth({
         .safeParse(credentials);
 
         if (parsedCredentials.success) {
+            console.log("parsedCredentials:", parsedCredentials);
             const { email, password } = parsedCredentials.data;
             const user = await getUser(email);
+            console.log("user:", user);
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
 
